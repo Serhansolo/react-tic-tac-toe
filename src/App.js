84926ceb8f9312144375d2504e9e1a15257d22aa
Serhan Uygur <serhan.uygur@gmail.com>
2020-02-18 01:00:39 +0100

    Lifted the 'game state' to yet a hight component.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,77 @@ import React from 'react';
 
 // import Components
 import { Board } from './components/Board';
+import calculateWinner from './helpers/calculateWinner';
 
 export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null)
+      }],
+      xIsNext: true
+    };
+  }
+  handleClick(i) {
+    // Make the squares state immutable
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+
+    // Check if there is a winner OR if the clicked square is already occupied.
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    // if not then add current players signature.
+    squares[i] = this.state.xIsNext ? 'X' : "O";
+
+    // create new state object from changed data.
+    const state = {
+      history: history.concat([{
+        squares: squares
+      }]),
+      xIsNext: !this.state.xIsNext
+    }
+
+    // update the state.
+    this.setState(state);
+  }
+
+  renderPlayerStatus() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+
+    // initial game status based on next player status.
+    let status = `Next Player: ${this.state.xIsNext ? 'X' : 'O'}`;
+
+    // if game has a winner then change status accordingly.
+    if (winner) {
+      status = `Winner is: ${winner}!`;
+    }
+
+    return status;
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+
     return (
       <div className="game">
-        <div className="game-board">
-          <Board />
+        <div className={`game-board ${!winner ? 'playing' : ''}`}>
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div className="status">
+            {this.renderPlayerStatus()}
+          </div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
--- src/components/Board.js
+++ src/components/Board.js
@@ -2,66 +2,20 @@ import React from 'react';
 
 // import Components
 import { Square } from './Square';
-import calculateWinner from '../helpers/calculateWinner';
 
 export class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true
-    };
-  }
-
-  handleClick(i) {
-    // Make the squares state immutable
-    const squares = this.state.squares.slice();
-
-    // Check if there is a winner OR if the clicked square is already occupied.
-    if (this.gameHasWinner() || squares[i]) {
-      return;
-    }
-
-    // if not then add current players signature.
-    squares[i] = this.state.xIsNext ? 'X' : "O";
-
-    // create new state object from changed data.
-    const state = {
-      squares: squares,
-      xIsNext: !this.state.xIsNext
-    }
-
-    // update the state.
-    this.setState(state);
-  }
-
   renderSquare(i) {
-    return <Square
-      value={this.state.squares[i]}
-      onClick={() => this.handleClick(i)}
-    />;
-  }
-
-  renderPlayerStatus() {
-    // initial game status based on next player status.
-    let status = `Next Player: ${this.state.xIsNext ? 'X' : 'O'}`;;
-
-    // if game has a winner then change status accordingly.
-    if (this.gameHasWinner()) {
-      status = `Winner is: ${this.gameHasWinner()}!`;
-    }
-
-    return status;
-  }
-
-  gameHasWinner() {
-    return calculateWinner(this.state.squares);
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
   }
 
   render() {
     return (
-      <div className={`${!this.gameHasWinner() ? 'playing' : ''}`}>
-        <div className="status">{this.renderPlayerStatus()}</div>
+      <div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
